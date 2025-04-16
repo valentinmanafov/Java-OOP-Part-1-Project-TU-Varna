a6ec9ec90a05bf1e34f1f2f113e1b0daaa49e1df
Valentin Manafov <itzbunnyfn@gmail.com>
2025-04-16 21:51:47 +0300

    Created Column class to represent table columns with name and DataType

---
+++ Project Files/src/Column.java
@@ -0,0 +1,22 @@
+public class Column {
+    private String name;
+    private DataType type;
+
+    public Column(String name, DataType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public DataType getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + type + ")";
+    }
+}
