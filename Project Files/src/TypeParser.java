5bcba89478d7960541a1b2d0f9ed43d3b2fcd884
Valentin Manafov <itzbunnyfn@gmail.com>
2025-04-24 23:33:38 +0300

    Creation of TypeParser class with initial parse method with error handling

---
+++ Project Files/src/TypeParser.java
@@ -0,0 +1,41 @@
+public class TypeParser {
+
+    public static Object parse(String value, DataType targetType) throws DatabaseOperationException {
+        if (value == null || value.equalsIgnoreCase("NULL")) {
+            return null;
+        }
+        try {
+            switch (targetType) {
+                case INTEGER:
+                    String intValue = value;
+                    if (value.startsWith("+")) {
+                        intValue = value.substring(1);
+                    }
+                    return Integer.parseInt(intValue);
+                case DOUBLE:
+                    String doubleValue = value;
+                    if (value.startsWith("+")) {
+                        doubleValue = value.substring(1);
+                    }
+                    return Double.parseDouble(doubleValue);
+                case STRING:
+                    if (value.startsWith("\"") && value.endsWith("\"") && value.length() >= 2) {
+                        return value.substring(1, value.length() - 1).replace("\\\"", "\"").replace("\\\\", "\\");
+                    }
+                    return value;
+                default:
+                    throw new DatabaseOperationException("Internal Error: Unsupported data type: " + targetType);
+            }
+        } catch (NumberFormatException e) {
+            throw new DatabaseOperationException("Cannot parse '" + value + "' as " + targetType, e);
+        }
+    }
+
+    public static boolean valueMatchesType(Object v, DataType t) {
+        return false;
+    }
+
+    public static boolean looselyEquals(Object tv, String sv, DataType ct) {
+        return false;
+    }
+}
